d475d57a719dd68acc9326d94e6c3c057a85228c
Vitaly Zakatov <answer-0885@mail.ru>
2022-03-24 22:44:51 +0700

    Работа с формами

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import telephoneList from './modules/telephoneList'
 import burgerMenu from './modules/burgerMenu'
 import telMask from './modules/telMask'
 import modal from './modules/modal'
+import sendForm from './modules/sendForm'
 
+modal('open-popup-1', 'close', 'popup-repair-types')
+modal('open-popup-2', 'close', 'popup-repair-types')
 
 telephoneList()
 burgerMenu()
 telMask()
-
-modal('open-popup-1', 'close', 'popup-repair-types')
-modal('open-popup-2', 'close', 'popup-repair-types')
+sendForm({
+  formID: 'feedback1'
+});
+sendForm({
+  formID: 'feedback2'
+});
+sendForm({
+  formID: 'feedback3'
+});
+sendForm({
+  formID: 'feedback4'
+});
+sendForm({
+  formID: 'feedback5'
+});
+sendForm({
+  formID: 'feedback6'
+});
